e74803155b5cc6bc12b6c5b61fda739a1eb580c6
krishnanc1986 <krishnanc.in@gmail.com>
2019-10-25 18:32:11 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AddProjectComponent} from '../app/add-project/add-project.component';
+import { AddUserComponent} from '../app/add-user/add-user.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'AddProject',component: AddProjectComponent},
+{path:'AddUser',component: AddUserComponent},
+{path: 'AddTask', component: AddTaskComponent},
+{path: 'ViewTask',component:ViewTaskComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProjectComponent,
+    AddUserComponent,
+    AddTaskComponent,
+    ViewTaskComponent
   ],
   imports: [
     BrowserModule,
