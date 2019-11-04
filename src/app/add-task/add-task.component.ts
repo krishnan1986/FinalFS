649cbb5aeb496de42c3e004915f295de7bd88db0
krishnanc1986 <krishnanc.in@gmail.com>
2019-11-04 16:28:39 +0530

    checkbox logic

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input, EventEmitter, Output} from '@angular/core';
 import {task} from '../model/Task';
 import { HttpClientService } from '../service/http-client.service';
+import { Project } from '../model/project';
+
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -11,6 +13,9 @@ export class AddTaskComponent implements OnInit {
  
  // model:task = new task("test",1,1,new Date(2013,12,1),new Date(2014,1,1));
  model:task= new task();
+ projectModel: Project = new Project();
+ @Input() isParentTask: boolean = false;
+ @Output() getChange = new EventEmitter();
   postUrl:string ="http://localhost:8112/"
 
   constructor( private httpClientService: HttpClientService) {
@@ -21,6 +26,14 @@ export class AddTaskComponent implements OnInit {
   ngOnInit() {
     console.log('inside add task comp');
   }
+
+  checkParent(isParentTask)
+  {
+    alert(isParentTask);
+    this.getChange.emit(this.isParentTask);
+  }
+
+  
 
   addTaskService():void  {
     //console.log(form.value)
---
+++ src/app/model/project.ts
@@ -0,0 +1,9 @@
+export class Project
+{
+
+    public projectname: String;
+    public projectID: String;
+    public StartDate: String;
+    public endDate: String;
+    public priority: number;
+}
